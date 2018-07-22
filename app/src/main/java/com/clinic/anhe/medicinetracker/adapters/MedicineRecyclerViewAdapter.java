bd250d5b68e405f401b24bf3fcee852a9040aa3f
chloe lai <chloe.jy.lai@gmail.com>
2018-07-22 15:50:07 +0800

    finish parts of medicine fragment

--- app/src/main/java/com/clinic/anhe/medicinetracker/adapters/MedicineRecyclerViewAdapter.java
+++ app/src/main/java/com/clinic/anhe/medicinetracker/adapters/MedicineRecyclerViewAdapter.java
@@ -1,12 +1,18 @@
 package com.clinic.anhe.medicinetracker.adapters;
 
+import android.content.Context;
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ToggleButton;
+import android.widget.CompoundButton;
 
 import com.clinic.anhe.medicinetracker.R;
 import com.clinic.anhe.medicinetracker.model.MedicineCardViewModel;
@@ -16,6 +22,8 @@ import java.util.List;
 public class MedicineRecyclerViewAdapter extends RecyclerView.Adapter<MedicineRecyclerViewAdapter.MedicineViewHolder> {
 
     private List<MedicineCardViewModel> medicineList;
+    private static Context mContext;
+
 
     public MedicineRecyclerViewAdapter(List<MedicineCardViewModel> medicineList) {
         this.medicineList = medicineList;
@@ -26,6 +34,7 @@ public class MedicineRecyclerViewAdapter extends RecyclerView.Adapter<MedicineRe
     public MedicineViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.cardview_medicine, parent, false);
+        mContext = parent.getContext();
         MedicineViewHolder medicineViewHolder = new MedicineViewHolder(view);
         return medicineViewHolder;
     }
@@ -36,6 +45,7 @@ public class MedicineRecyclerViewAdapter extends RecyclerView.Adapter<MedicineRe
         holder.imageIcon.setImageResource(current.getMedicineIcon());
         holder.medicineName.setText(current.getMedicinName());
         holder.medicineId.setText(current.getMedicineId());
+
     }
 
     @Override
@@ -47,12 +57,37 @@ public class MedicineRecyclerViewAdapter extends RecyclerView.Adapter<MedicineRe
         ImageView imageIcon;
         TextView medicineName;
         TextView medicineId;
+        ImageButton imageButton;
+
+        AnimatedVectorDrawable mAddDrawable;
+        AnimatedVectorDrawable mCheckDrawable;
+        boolean addButtonClicked = false;
 
         public MedicineViewHolder(View itemView) {
             super(itemView);
             imageIcon = itemView.findViewById(R.id.medicine_icon);
             medicineName = itemView.findViewById(R.id.medicine_name);
             medicineId = itemView.findViewById(R.id.medicine_id);
+            imageButton = itemView.findViewById(R.id.medicine_add_button);
+
+            mAddDrawable = (AnimatedVectorDrawable) mContext.getDrawable(R.drawable.ic_add_animatable);
+
+            //setOnClickListener
+            imageButton.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    //once the medicine item is add, you cannot remove it till next fragment
+                    if(addButtonClicked) {
+                        //do nothing
+                    }else {
+                        imageButton.setImageDrawable(mAddDrawable);
+                        mAddDrawable.start();
+                    }
+                    addButtonClicked = true;
+                }
+            });
+
 
         }
     }
